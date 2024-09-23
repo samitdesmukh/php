149beeac11c0618898428d15be2d965277b49ed5
samit <desmukhkumarsamit@gmail.com>
2024-09-23 23:15:12 +0530

    login

--- class1/login.php
+++ class1/login.php
@@ -2,18 +2,18 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
 
-    // Checking if the entered username is "admin"
+    
     if ($username === "admin") {
-        // Reverse the username
+        
         $reversedUsername = strrev($username);
-        // Display the welcome message
+        
         echo "Welcome " . $reversedUsername;
     } else {
-        // Display error message for incorrect username
+        
         echo "Invalid username. Please try again.";
     }
 } else {
-    // If the script is accessed without POST data, redirect to the login page
+   
     header("Location: index.html");
     exit();
 }
